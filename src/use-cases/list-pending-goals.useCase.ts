f8020f8fa7da1d05e60b8e7c23877e2a251d40c1
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-10 18:52:19 -0300

    Feat: List all pending goals refer to the current week

---
+++ src/use-cases/list-pending-goals.useCase.ts
@@ -0,0 +1,62 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, count, gte, lte, eq, sql } from 'drizzle-orm'
+import { number } from 'zod'
+
+dayjs.extend(weekOfYear)
+
+export async function getWeekPendingGoals() {
+  // Deve retornar sempre da semana atual.
+  const lastDayOffWeek = dayjs().endOf('week').toDate()
+  const firstDayOffWeek = dayjs().startOf('week').toDate()
+
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.created_at,
+      })
+      .from(goals)
+      .where(lte(goals.created_at, lastDayOffWeek))
+  )
+
+  const goalsCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.goalId).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOffWeek),
+          lte(goalCompletions.createdAt, lastDayOffWeek)
+        )
+      )
+      .groupBy(goalCompletions.id)
+  )
+
+  const pendingGoals = await db
+    .with(goalsCreatedUpToWeek, goalsCompletionCounts)
+    .select({
+      id: goalsCreatedUpToWeek.id,
+      title: goalsCreatedUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+      completionCount: sql /* sql */`
+      COALESCE(${goalsCompletionCounts.completionCount}, 0)
+      `.mapWith(Number),
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(
+      goalsCompletionCounts,
+      eq(goalsCompletionCounts.goalId, goalsCreatedUpToWeek.id)
+    )
+
+  return {
+    pendingGoals,
+  }
+}

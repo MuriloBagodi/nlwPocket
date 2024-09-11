c16c3a53484f932fdb8c3e958ae42ca02ff6a97b
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-11 17:17:47 -0300

    Refac: rm unnecessary import from zod

--- src/use-cases/list-pending-goals.useCase.ts
+++ src/use-cases/list-pending-goals.useCase.ts
@@ -3,7 +3,6 @@ import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 import { and, count, gte, lte, eq, sql } from 'drizzle-orm'
-import { number } from 'zod'
 
 dayjs.extend(weekOfYear)
 
